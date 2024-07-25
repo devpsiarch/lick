e0bfb7fe13c78a820fe57750d05ac900625eb1dd
devpsiarch <hmayoubaty@gmail.com>
2024-07-25 16:00:53 +0100

    a lot ik

--- lick.go
+++ lick.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha1"
 	"compress/zlib"
 	"io"
 	"os"
@@ -48,7 +49,6 @@ func cat_file(option,blob_sha string){
 	//for now it has to be -p
 
 	path := fmt.Sprintf(".git/objects/%v/%v", blob_sha[0:2], blob_sha[2:])
-	fmt.Println("the path is ",path)
 	file,err := os.Open(path)
 	if(err != nil){
 		fmt.Println("cant open file ",path)
@@ -65,18 +65,49 @@ func cat_file(option,blob_sha string){
 		return
 	}
 	parts := strings.Split(string(str), "\x00")
+	if(option == "-p"){
+		fmt.Println(parts[1])
+		return
+	}
+	parts = strings.Split(parts[0]," ")
 	// p,1 => content || t,first of 0 || s,second of 0 |> still there is more option will be implimented later
 
 	switch option {
-	case "-p": 
-		fmt.Print(parts[1])
 	case "-t":
-		fmt.Print(parts[0])
+		fmt.Println(parts[0])
 	case "-s":
-		fmt.Print(parts[0])
+		fmt.Println(parts[1])
 	}
 	r.Close()
 	
+}
+
+
+func hash_object(option,filepath string){
+	if(option != "-w"){
+		fmt.Println("wrong option only <-w> for now!")
+		return
+	}
+	//getting the input of the sha1 fuc
+	
+	file,err := os.ReadFile(filepath)
+	if(err != nil){
+		fmt.Println("unable to read file :",filepath)
+		return
+	}
+
+	stat,err := os.Stat(filepath)
+	if(err != nil){
+		fmt.Println("unable to get stats of file :",filepath)
+		return
+	}
+	content := string(file)
+	header := fmt.Sprintf("blob %d\x00%s", stat.Size(), content)
+
+	sha := (sha1.Sum([]byte(header)))
+	hash := fmt.Sprintf("%x",sha)
+	fmt.Println("the hash is :",hash)
+
 }
 
 func main(){
@@ -84,18 +115,20 @@ func main(){
 		fmt.Println("invalid command : lick <command> <option>")
 		return
 	}
-	//creating .lick and deleting it 
-	if(os.Args[1] == "init"){
-		init_lick()
+	switch os.Args[1] {
+		case "init":
+			//creating .lick 
+			init_lick()
+		case "clean":
+			//cleaning .lick
+			clean_lick()
+		case "cat-file":
+			//reading objects
+			cat_file(os.Args[2],os.Args[3])
+		case "hash-object":
+			//writing object
+			hash_object(os.Args[2],os.Args[3])
 	}
-	if(os.Args[1] == "clean"){
-		clean_lick()
-	}
-	//lick cat-file -p <hash>
-	if(os.Args[1] == "cat-file"){
-		cat_file(os.Args[2],os.Args[3])	
-	}
-
 
 
 
